aae2ff942a5666d190bca85d08efb3557deeea40
Russell Gordon <rgordon@lcs.on.ca>
2021-10-07 07:06:57 -0400

    Added QuoteService whose role it is to interact with the API and decode the JSON into an array of quotes.

---
+++ SwiftUI Starter/Feature/Quotes/Service/QuotesService.swift
@@ -0,0 +1,29 @@
+//
+//  QuotesService.swift
+//  SwiftUI Starter
+//
+//  Created by Russell Gordon on 2021-10-07.
+//
+
+import Foundation
+
+protocol QuotesService {
+    func fetch() async throws -> [Quote]
+}
+
+final class QuotesServiceImplementation: QuotesService {
+    
+    final func fetch() async throws -> [Quote] {
+        
+        // Start a URL session to interact with the API
+        let urlSession = URLSession.shared
+        // Assemble the URL that points to the JSON endpoint
+        let url = URL(string: APIConstants.baseURL.appending("/api/quotes"))
+        // Fetch the raw data
+        let (data, _) = try await urlSession.data(from: url!)
+        // Attempt to decode and return the array of quotes
+        return try JSONDecoder().decode([Quote].self, from: data)
+        
+    }
+    
+}
