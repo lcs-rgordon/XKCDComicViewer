8c1768a05f56f1ad5ab65203b5272d5d28aa662e
Russell Gordon <rgordon@lcs.on.ca>
2021-10-07 06:38:53 -0400

    Added Quote model.

---
+++ SwiftUI Starter/Feature/Quotes/Model/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  SwiftUI Starter
+//
+//  Created by Russell Gordon on 2021-10-07.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let anime: String
+    let character: String
+    let quote: String
+}
