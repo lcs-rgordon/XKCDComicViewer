ea858aa6e6e88c34f0b3b9810c9f510f93de8a73
Russell Gordon <rgordon@lcs.on.ca>
2021-10-07 06:36:16 -0400

    Initial Commit

---
+++ SwiftUI Starter/SwiftUI_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StarterApp.swift
+//  SwiftUI Starter
+//
+//  Created by Russell Gordon on 2021-10-07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
