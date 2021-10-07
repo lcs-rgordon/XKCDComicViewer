4eb1df932eb0697ec4a5c2e487123a6bd132d14c
Russell Gordon <rgordon@lcs.on.ca>
2021-10-07 06:37:25 -0400

    Added API constants enum to track shared constants we will need in the app.

---
+++ SwiftUI Starter/APIConstants/APIConstants.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstants.swift
+//  SwiftUI Starter
+//
+//  Created by Russell Gordon on 2021-10-07.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let baseURL = "https://animechan.vercel.app"
+}
